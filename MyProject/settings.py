5cbb0fbd30f9bc521e49a04fbff55336022eea12
Amit Vishwakarma <amitkarma18@gmail.com>
2026-04-17 13:56:41 +0530

    Add WHITENOISE_MANIFEST_STRICT = False

--- MyProject/settings.py
+++ MyProject/settings.py
@@ -124,6 +124,7 @@ STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS=[STATIC_DIR]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+WHITENOISE_MANIFEST_STRICT = False
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
